334bca25986a39fcaa3200523b7ae6cc8b69c96b
Ilya Obukhov <mesisleo10@gmail.com>
2018-09-19 14:19:34 +0300

    end part 4

--- django_tutorial/mysite/polls/views.py
+++ django_tutorial/mysite/polls/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.template import loader
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Choice ,Question
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
-    #output = ', '.join([q.question_text for q in latest_question_list])
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = f'Youre lokking at the results of question {question_id}'
-    return HttpResponse(f'response {question_id}')
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse(f'Youre voting on question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didnt select a choice',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
